7c926ad6e3ed599f01f06ee59dd84081d96211a2
DanoR <zheung@vip.qq.com>
2020-09-25 17:07:58 +0800

    up

---
+++ side/find.js
@@ -0,0 +1,50 @@
+require('../env');
+
+const dataE = require('../data/BaseData/en_us.json');
+const dataZ = require('../data/BaseData/zh_cn.json');
+
+const ver = '09.12';
+const names = `
+铁铠冥魂
+暗星尊 烬
+暗星 卡尔玛
+暗星 萨科
+`.trim().split('\n');
+
+const result = [];
+const ids = [];
+
+let line1 = ver;
+let line2 = '';
+
+for(const name of names) {
+	for(const hero of Object.values(dataZ)) {
+		for(const skin of Object.values(hero.skins)) {
+			if((skin.name || '') == name) {
+				const idStr = `${String(hero.id).padStart(3, '0')}${String(skin.id).padStart(3, '0')}`;
+				const nameSkinZ = skin.name.trim();
+				const nameSkinE = dataE[hero.id].skins[skin.id].name.trim();
+
+				result.push(`\t${idStr}\t${nameSkinE}`);
+				result.push(`\t\t${nameSkinZ}`);
+
+				line1 += `\t${nameSkinE}`;
+				line2 += `\t${nameSkinZ}`;
+
+				ids.push(idStr);
+			}
+		}
+	}
+}
+
+result.push('-------');
+result.push(`["${ver}", ${ids.map(id => `"${id}"`).join(', ')}]`);
+result.push('-------');
+result.push(line1.trimEnd());
+result.push(line2.trimEnd());
+
+result[0] = ver + result[0];
+
+_fs.writeFileSync('./_final/chromas.txt', result.join('\n'));
+
+require('./chromas');
